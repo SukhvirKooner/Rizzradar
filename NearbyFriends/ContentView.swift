bae117cce04a9cda504f2b91484ea229d29bfe21
SukhvirKooner <sukhvirkooner05@gmail.com>
2025-04-05 16:52:23 +0530

    Initial Commit

---
+++ NearbyFriends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearbyFriends
+//
+//  Created by Sukhvir Singh on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearbyFriends/NearbyFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyFriendsApp.swift
+//  NearbyFriends
+//
+//  Created by Sukhvir Singh on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct NearbyFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
